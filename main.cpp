d2933824272b3f3a1a61722935493835564df39e
Christopher Jarrell <78067896+LordEmbrion@users.noreply.github.com>
2021-10-26 16:21:35 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main () {
+    int favoriteNumber;
+    std::cout << "Please enter a number between 1 - 100: ";
+    std::cin >> favoriteNumber;
+    if (favoriteNumber > 100 || favoriteNumber < 0){
+        std::cout << "You were always the teacher's least favorite...\n";
+    }
+    else {
+        std::cout << "Nice job! You can follow instructions!\n";
+    }
+    return 0;
+}
